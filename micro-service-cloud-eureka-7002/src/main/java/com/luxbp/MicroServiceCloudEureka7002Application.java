38136dc1cea7f5dbacd0c914aadac62a1c2bae65
dustwh <32893851+dustwh@users.noreply.github.com>
2023-01-23 23:48:30 -0500

    clean

--- micro-service-cloud-eureka-7002/src/main/java/com/luxbp/MicroServiceCloudEureka7002Application.java
+++ micro-service-cloud-eureka-7002/src/main/java/com/luxbp/MicroServiceCloudEureka7002Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer //Open the Eureka server and accept the registration of other microservices / 开启 Eureka server,接受其他微服务的注册
-public class MicroServiceCloudEureka7001Application {
+public class MicroServiceCloudEureka7002Application {
     public static void main(String[] args) {
-        SpringApplication.run(MicroServiceCloudEureka7001Application.class, args);
+        SpringApplication.run(MicroServiceCloudEureka7002Application.class, args);
     }
 }
--- micro-service-cloud-eureka-7003/src/main/java/com/luxbp/MicroServiceCloudEureka7003Application.java
+++ micro-service-cloud-eureka-7003/src/main/java/com/luxbp/MicroServiceCloudEureka7003Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer //Open the Eureka server and accept the registration of other microservices / 开启 Eureka server,接受其他微服务的注册
-public class MicroServiceCloudEureka7001Application {
+public class MicroServiceCloudEureka7003Application {
     public static void main(String[] args) {
-        SpringApplication.run(MicroServiceCloudEureka7001Application.class, args);
+        SpringApplication.run(MicroServiceCloudEureka7003Application.class, args);
     }
 }
