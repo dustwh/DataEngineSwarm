ad64371eb280781a94391dc697f72c0ac08d1805
dustwh <32893851+dustwh@users.noreply.github.com>
2023-02-03 03:20:51 -0500

    default fallback

--- micro-service-cloud-consumer-dept-feign/src/main/java/com/luxbp/controller/HystrixController_Consumer.java
+++ micro-service-cloud-consumer-dept-feign/src/main/java/com/luxbp/controller/HystrixController_Consumer.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 @Slf4j
 @RestController
+@DefaultProperties(defaultFallback = "dept_Global_FallbackMethod") //全局的服务降级方法
 public class HystrixController_Consumer {
     @Resource
     private DeptHystrixService deptHystrixService;
@@ -19,7 +20,8 @@ public class HystrixController_Consumer {
     }
     //在客户端进行降级
     @RequestMapping(value = "/consumer/dept/hystrix/timeout/{id}")
-    @HystrixCommand(fallbackMethod = "dept_TimeoutHandler") //为该请求指定专属的回退方法
+//    @HystrixCommand(fallbackMethod = "dept_TimeoutHandler") //为该请求指定专属的回退方法
+    @HystrixCommand //use default fallback
     public String deptInfo_Timeout(@PathVariable("id") Integer id) {
         String s = deptHystrixService.deptInfo_Timeout(id);
         log.info(s);
@@ -29,5 +31,14 @@ public class HystrixController_Consumer {
     public String dept_TimeoutHandler(@PathVariable("id") Integer id) {
         log.info("deptInfo_Timeout error，service has been fallback！");
         return "system busy（client's deptInfo_Timeout has been triggered）";
+    }
+
+    /**
+     * 全局的 fallback 方法，
+     * 回退方法必须和 hystrix 的执行方法在相同类中
+     * @DefaultProperties(defaultFallback = "dept_Global_FallbackMethod") 类上注解，请求方法上使用 @HystrixCommand 注解
+     */
+    public String dept_Global_FallbackMethod() {
+        return "default fall back";
     }
 }
