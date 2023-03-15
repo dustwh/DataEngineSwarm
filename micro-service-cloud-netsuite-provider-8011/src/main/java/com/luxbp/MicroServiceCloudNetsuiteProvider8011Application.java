f1a5a5e948ecf5b7253b162dd33367dd5686d88b
dustwh <32893851+dustwh@users.noreply.github.com>
2023-03-15 10:18:49 -0400

    depend main and config of netsuite provider

---
+++ micro-service-cloud-netsuite-provider-8011/src/main/java/com/luxbp/MicroServiceCloudNetsuiteProvider8011Application.java
@@ -0,0 +1,12 @@
+package com.luxbp;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroServiceCloudNetsuiteProvider8011Application {
+    public static void main(String[] args) {
+
+    }
+}
