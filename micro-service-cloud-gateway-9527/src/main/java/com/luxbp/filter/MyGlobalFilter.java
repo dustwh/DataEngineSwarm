941c5e458576b40313b4c3ebbd8e7bda7b4de857
dustwh <32893851+dustwh@users.noreply.github.com>
2023-03-15 10:17:07 -0400

    fix gateway proxy chain

--- micro-service-cloud-gateway-9527/src/main/java/com/luxbp/filter/MyGlobalFilter.java
+++ micro-service-cloud-gateway-9527/src/main/java/com/luxbp/filter/MyGlobalFilter.java
@@ -1,35 +1,35 @@
-package com.luxbp.filter;
-
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.cloud.gateway.filter.GatewayFilterChain;
-import org.springframework.cloud.gateway.filter.GlobalFilter;
-import org.springframework.core.Ordered;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Component;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
-import java.util.Date;
-
-/**
- * show case of modifying GlobalFilter here
- */
-@Component
-@Slf4j
-public class MyGlobalFilter implements GlobalFilter, Ordered {
-    @Override
-    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("enter modified global filter MyGlobalFilter" + new Date());
-        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
-        if (uname == null) {
-            log.info("query param 'uname' can not be null！");
-            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
-            return exchange.getResponse().setComplete();
-        }
-        return chain.filter(exchange);
-    }
-    @Override
-    public int getOrder() {
-        //过滤器的顺序，0 表示第一个
-        return 0;
-    }
-}
+//package com.luxbp.filter;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+//import org.springframework.cloud.gateway.filter.GlobalFilter;
+//import org.springframework.core.Ordered;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.server.ServerWebExchange;
+//import reactor.core.publisher.Mono;
+//import java.util.Date;
+//
+///**
+// * show case of modifying GlobalFilter here
+// */
+//@Component
+//@Slf4j
+//public class MyGlobalFilter implements GlobalFilter, Ordered {
+//    @Override
+//    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+//        log.info("enter modified global filter MyGlobalFilter" + new Date());
+//        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+//        if (uname == null) {
+//            log.info("query param 'uname' can not be null！");
+//            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+//            return exchange.getResponse().setComplete();
+//        }
+//        return chain.filter(exchange);
+//    }
+//    @Override
+//    public int getOrder() {
+//        //过滤器的顺序，0 表示第一个
+//        return 0;
+//    }
+//}
