77f3682a3b3f01fe533824c98112de716c60959b
dustwh <32893851+dustwh@users.noreply.github.com>
2023-03-16 19:14:56 -0400

    Revert "maven compiler and lombok version switch"
    This reverts commit 3c25d13352e968506e66005a96d6c41bfc21348f.

--- micro-service-cloud-eureka-7002/src/main/java/com/luxbp/MicroServiceCloudEureka7002Application.java
+++ micro-service-cloud-eureka-7002/src/main/java/com/luxbp/MicroServiceCloudEureka7002Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
-@EnableEurekaServer //Open the Eureka server and accept the registration of other microservices
+@EnableEurekaServer //Open the Eureka server and accept the registration of other microservices / 开启 Eureka server,接受其他微服务的注册
 public class MicroServiceCloudEureka7002Application {
     public static void main(String[] args) {
         SpringApplication.run(MicroServiceCloudEureka7002Application.class, args);
